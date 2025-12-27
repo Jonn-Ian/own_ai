87185d27b764593e243b455f029ee909112c1e28
Jonn-Ian <jonniansabar@gmail.com>
2025-12-27 15:41:57 +0800

    update

--- forecasting/ARIMA_1/ARIMA.py
+++ forecasting/ARIMA_1/ARIMA.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from AR.p import p
-from I.d import d
+from .AR.p import p
+from .I.d import d
 
 path_file = r"E:\Git\my_repos\own_ai\forecasting\csv\cleansed.csv"
 df = pd.read_csv(path_file).sort_values("year")
